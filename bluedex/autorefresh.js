8131f0f6490e9df9e780601ad41eaa89a57dcf24
laplongejunior <thierry.ladeuze@yahoo.fr>
2023-05-12 12:21:26 +0200

    Update autorefresh.js

--- bluedex/autorefresh.js
+++ bluedex/autorefresh.js
@@ -1,5 +1,5 @@
 // IIFE in strict mode to be able to use unstrict code at the start of execution, yet have most of the code execute as strict
-(function(global) { // I prefer getting the global object with "this" rather than using the name 'window', personal taste
+(function(global) { // I prefer getting the global object with unstrict "this" rather than using the name 'window', personal taste
     "use strict";
 
     // To allow easy redirects
@@ -9,7 +9,7 @@
 	// My old scripts import this as a seperate js file for easier updating
 	// For this reddit comment I merged the files and removed some complex feature like slowing down the observer
 	// But I'm not sure it will work exactly the same
-    const UTILS = {
+    	const UTILS = {
 		// Basically calls it everytime there's a change + one time when added
 		// My personal version only triggers the callback if no event triggered during 3s, but for such a simple script it would be overkill
 		detectUpdate: (parent, callback) => {
@@ -24,26 +24,26 @@
 		// As a security, this polyfill makes it so that querySelector returns null in case of multiple matches
 		querySelectorSafe: function(element, selector) {
 			console.warn(element);
-		  const result = element.querySelectorAll(selector);
-		  if (result.length == 1) return result.item(0);
-		  if (result.length > 1) {
-			global.console.warn("Several matches found for querySelector! Discarding...");
-			global.console.warn(result);
-		  }
-		  return null;
+		  	const result = element.querySelectorAll(selector);
+		  	if (result.length == 1) return result.item(0);
+		  	if (result.length > 1) {
+				global.console.warn("Several matches found for querySelector! Discarding...");
+				global.console.warn(result);
+		  	}
+		  	return null;
 		}
 	};
 
-    // If there's a DOM modification, schedule a new try
+    	// If there's a DOM modification, schedule a new try
 	const parent = doc.body; // PERF: Use querySelectorSafe to identify a more precise parent element
 	console.warn("Script loaded!");
 	console.warn(parent);
 	
-    const observer = UTILS.detectUpdate(parent, ()=>{
+   	const observer = UTILS.detectUpdate(parent, ()=>{
 		console.warn("Update detected!");
 		// Courtesy of u/Hakorr
 		if (!UTILS.querySelectorSafe(parent, 'h2.alert')) return;
-        observer.disconnect();
+        	observer.disconnect();
 		global.location.reload();
-    });
+    	});
 })(this);
